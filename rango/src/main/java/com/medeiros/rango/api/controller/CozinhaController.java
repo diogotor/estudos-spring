524aa6a4d15d96d87d6ee92d6f628d91624c37d8
diogomedeiros <diogomedeiros@recife.pe.gov.br>
2022-05-07 10:21:38 -0300

    ID #20220507

---
+++ rango/src/main/java/com/medeiros/rango/api/controller/CozinhaController.java
@@ -0,0 +1,29 @@
+package com.medeiros.rango.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.medeiros.rango.domain.model.Cozinha;
+import com.medeiros.rango.domain.repository.CozinhaRepository;
+
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+	
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+	
+	@GetMapping
+	public List<Cozinha> listar(){
+		return cozinhaRepository.listar();
+	}
+	
+	
+	
+	
+}
